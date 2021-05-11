0f22d330ac6912ffbfc14503eb3fe7c51f182924
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2021-05-12 00:45:02 +0900

    Add liff sample.

---
+++ src/main/java/org/ichigojam/memberapp/web/LIFFController.java
@@ -0,0 +1,16 @@
+package org.ichigojam.memberapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LIFFController {
+
+    @GetMapping("/liff")
+    public String hello(Model model) {
+        // [[${test}]] の部分を Hello... で書き換えて、liff.htmlを表示する
+        model.addAttribute("test", "Hello Tymeleaf!");
+        return "liff";
+    }
+}
